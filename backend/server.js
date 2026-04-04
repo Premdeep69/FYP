7d0f060838fe65df12099f356b5597aeed695fc7
Premdeep <premdiprai1234@gmail.com>
2026-04-04 18:35:50 +0545

    add server entrypoint fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import app from "./app.js";
+import dotenv from "dotenv";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import app from "./app.js";
 import { handleConnection } from "./socket/socketHandler.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,10 +11,9 @@ const PORT = process.env.PORT || 5000;
 // Create HTTP server
 const server = createServer(app);
 
-// Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: true, // Allow all origins in development
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
