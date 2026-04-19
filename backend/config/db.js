7989fb8e4d87cf5aa9e8355b2750edd306769cd5
Premdeep <premdiprai1234@gmail.com>
2026-04-19 11:22:47 +0545

    add log for mongose

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+    });
+    console.log("MongoDB connected", process.env.MONGO_URI);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
